b6ec0c6ee194f847a9104323b0855fbbcb9db5da
Bernardo A. Dal Corno <bernardoadc@gmail.com>
2024-07-26 15:26:54 -0300

    feat: multiplos modelos

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 import requests
 from PIL import Image
-from transformers import AutoProcessor, BlipForConditionalGeneration
+from transformers import BlipForConditionalGeneration, AutoProcessor
 import gradio as gr
 
-# BlipProcessor
-# This is a processor class that is used for preprocessing data for the BLIP model.
-# It wraps a BLIP image processor and an OPT/T5 tokenizer into a single processor.
-# This means it can handle both image and text data, preparing it for input into the BLIP model.
+# Função para carregar o modelo
+def load_model(model_name):
+    if model_name == "Salesforce BLIP":
+        processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+        model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+    # elif model_name == "EVA-CLIP":
+    #     processor = AutoProcessor.from_pretrained("ViTAE-Transformer/EVA")
+    #     model = BlipForConditionalGeneration.from_pretrained("ViTAE-Transformer/EVA")
+    # elif model_name == "EVCap":
+    #     processor = AutoProcessor.from_pretrained("Jiaxuan-Li/EVCap")
+    #     model = BlipForConditionalGeneration.from_pretrained("Jiaxuan-Li/EVCap")
+    elif model_name == "BLIP-2 FlanT5-XXL":
+        processor = AutoProcessor.from_pretrained("salesforce/blip2-flan-t5-xxl")
+        model = BlipForConditionalGeneration.from_pretrained("salesforce/blip2-flan-t5-xxl")
+    return processor, model
 
-# BlipForConditionalGeneration
-# This is a model class that is used for conditional text generation given an image and an optional text prompt.
-# In other words, it can generate text based on an input image and an optional piece of text.
-# This makes it useful for tasks like image captioning or visual question answering, where
-# the model needs to generate text that describes an image or answer a question about an image.
-
-# Load the pretrained processor and model
-processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
-
-def generate_caption(image):
+# Função para gerar a legenda
+def generate_caption(image, model_name):
+    processor, model = load_model(model_name)
     # Convert the image to RGB format
     image = image.convert('RGB')
     text = "the image of"
@@ -32,13 +35,16 @@ def generate_caption(image):
 
     return caption
 
-# Create a Gradio interface
+# Interface do Gradio
 iface = gr.Interface(
     fn=generate_caption,
-    inputs=gr.Image(type="pil", label="Upload Image"),
+    inputs=[
+        gr.Image(type="pil", label="Upload Image"),
+        gr.Radio(["Salesforce BLIP", "BLIP-2 FlanT5-XXL"], label="Choose Model") # , "EVA-CLIP", "EVCap"
+    ],
     outputs=gr.Textbox(label="Generated Caption"),
-    title="Image Captioning with BLIP",
-    description="Upload an image and get a generated caption using the BLIP model."
+    title="Image Captioning with Multiple Models",
+    description="Upload an image and choose a model to get a generated caption."
 )
 
 # Launch the interface
